2c10722ba6fa1fc6cf4b6687d0e0e19229db4320
Prasanth Dendukuri <63752034+pd494@users.noreply.github.com>
2026-09-13 13:35:38 -0700

    refactor: use Photon connection boundary

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import { Effect } from "effect";
-import { createPhotonApp, PhotonConfig } from "./config";
+import { connectPhoton } from "./config";
 import { type MessageHandler, recvMessage } from "./connection";
 
 /** Run Satchel with the supplied message-processing behavior. */
 export const runApp = Effect.fn("Satchel.run")(function* <E, R>(
   sendMessage: MessageHandler<E, R>,
 ) {
-  const config = yield* PhotonConfig;
-  const app = yield* createPhotonApp(config);
+  const app = yield* connectPhoton();
 
   yield* Effect.logInfo("satchel.ready");
   yield* recvMessage(app, sendMessage);
